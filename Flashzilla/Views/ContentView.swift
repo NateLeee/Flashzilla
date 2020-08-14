51b290bb8008771fec588f0704524867ec4e584e
Nate Lee <nateleee.dev@gmail.com>
2020-08-14 14:26:22 +0800

    - [x] How to be notified when your SwiftUI app moves to the background.
    1. onReceive()
    2. NotificationCenter.default.publisher(for: UIApplication.xxx)

--- Flashzilla/Views/ContentView.swift
+++ Flashzilla/Views/ContentView.swift
@@ -10,21 +10,35 @@ import SwiftUI
 
 
 struct ContentView: View {
-    let timer = Timer.publish(every: 1, tolerance: 0.5, on: .main, in: .common, options: nil).autoconnect()
-    
-    @State var counter = 0
+    @State private var text = ""
+    @State private var yOffset: CGFloat = 0
     
     var body: some View {
         VStack {
-            Text("\(counter)")
-                .onReceive(timer) { (time) in
-                    if (self.counter >= 5) {
-                        self.timer.upstream.connect().cancel()
-                        
-                    } else {
-                        print("The time is now \(time)")
-                        self.counter += 1
-                    }
+            Text("Hello, World!")
+                .padding()
+            
+            TextField("Test", text: $text)
+                .padding()
+                .offset(x: 0, y: yOffset)
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification)) { _ in
+            print("⬅️ Moving to the background!")
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.willEnterForegroundNotification)) { (_) in
+            print("➡️ Moving back to the foreground!")
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.userDidTakeScreenshotNotification)) { (_) in
+            print("🌻 User took a screenshot!")
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.keyboardDidShowNotification)) { (_) in
+            withAnimation {
+                self.yOffset = -30
+            }
+        }
+        .onReceive(NotificationCenter.default.publisher(for: UIApplication.keyboardWillHideNotification)) { (_) in
+            withAnimation {
+                self.yOffset = 0
             }
         }
     }
