dd34bae8adb1f8ff1578b75d77a7aea3b09653f8
Nate Lee <nateleee.dev@gmail.com>
2020-08-14 13:27:55 +0800

    - [x] Making vibrations with UINotificationFeedbackGenerator and Core Haptics.

--- Flashzilla/Views/ContentView.swift
+++ Flashzilla/Views/ContentView.swift
@@ -7,131 +7,75 @@
 //
 
 import SwiftUI
+import CoreHaptics
+
 
 struct ContentView: View {
-    @State private var currentAmount: CGFloat = 0
-    @State private var finalAmount: CGFloat = 1
-    
-    @State private var currentAngle: Angle = .degrees(0)
-    @State private var finalAngle: Angle = .degrees(0)
-    
-    // how far the circle has been dragged
-    @State private var offset = CGSize.zero
-    // whether it is currently being dragged or not
-    @State private var isDragging = false
+    @State private var engine: CHHapticEngine?
     
     var body: some View {
-        // a drag gesture that updates offset and isDragging as it moves around
-        let dragGesture = DragGesture()
-            .onChanged { value in
-                self.offset = value.translation
-        }
-        .onEnded { _ in
-            withAnimation {
-                self.offset = .zero
-                self.isDragging = false
-            }
-        }
+        Text("Hello, World!")
+            .onAppear(perform: prepareHaptics)
+            .onTapGesture(perform: complexSuccess)
         
-        // a long press gesture that enables isDragging
-        let pressGesture = LongPressGesture()
-            .onEnded { value in
-                withAnimation {
-                    self.isDragging = true
-                }
-        }
-        
-        // a combined gesture that forces the user to long press then drag
-        let combined = pressGesture.sequenced(before: dragGesture)
-        
-        return VStack {
-            Text("Double Tap Me")
-                .onTapGesture(count: 2) {
-                    print("Double tapped!")
-            }
-            .padding()
-            
-            Text("Long Press Me")
-                .onLongPressGesture(minimumDuration: 1.5, pressing: { (pressing) in
-                    print("In progress: \(pressing)!\n")
-                }, perform: {
-                    print("Long pressed!")
-                })
-                .padding()
-            
-            Text("Zoom / Pinch me")
-                .scaleEffect(finalAmount + currentAmount) // So it can animate
-                .gesture(
-                    MagnificationGesture()
-                        .onChanged { amount in
-                            print(amount)
-                            self.currentAmount = amount - 1 // 'cause it starts with 1
-                    }
-                    .onEnded { amount in
-                        self.finalAmount += self.currentAmount
-                        self.currentAmount = 0 // Reset
-                        print("\n")
-                    }
-            )
-                .padding()
-            
-            // MARK: - Rotate Gesture
-            Text("Rotate Me")
-                .padding()
-                .frame(maxWidth: .infinity)
-                .border(Color.red, width: 1)
-                .rotationEffect(finalAngle + currentAngle)
-                .gesture(
-                    RotationGesture()
-                        .onChanged { (angle) in
-                            self.currentAngle = angle
-                            print(angle)
-                    }
-                    .onEnded { (angle) in
-                        // print(angle)
-                        self.finalAngle += self.currentAngle
-                        self.currentAngle = .zero
-                        print("\n")
-                    }
-            )
-            
-            
-            // MARK: - Gesture Clash Example
-            VStack {
-                Text("Text inside VStack")
-                    .onTapGesture {
-                        print("Text tapped")
-                }
-            }
-            .padding()
-            .highPriorityGesture(TapGesture().onEnded({ _ in
-                print("VStack tapped")
-            }))
-            
-            // MARK: - Simultaneous Gesture Example
-            VStack {
-                Text("Text inside VStack")
-                    .onTapGesture {
-                        print("Text tapped")
-                }
-            }
-            .padding()
-            .simultaneousGesture(TapGesture().onEnded({ _ in
-                print("VStack tapped")
-            }))
-            
-            // MARK: - Gesture Sequencing
-            Circle()
-                .fill(Color.red)
-                .frame(width: 64, height: 64)
-                .scaleEffect(isDragging ? 1.5 : 1)
-                .offset(offset)
-                .gesture(combined)
-            
-        }
-        .padding()
-        .border(Color.blue, width: 1)
     }
+    
+    // Custom funcs
+    func simpleSuccess() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            print("😭 This device isn't capable of providing haptic feedback.")
+            return
+        }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexSuccess() {
+        // make sure that the device supports haptics
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        var events = [CHHapticEvent]()
+        
+        // create one intense, sharp tap
+        //        let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+        //        let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 1)
+        //        let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 0)
+        //        events.append(event)
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        
+        
+        // convert those events into a pattern and play it immediately
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
